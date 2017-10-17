11e585590979430da830fa6916229c80d6bcb688
TakingTheL <yliu6@stuy.edu>
2017-10-16 23:26:33 -0400

    cleaned up code and added comments. Sorry for doing this so late, Tiffany :'(

--- stu_mean.py
+++ stu_mean.py
@@ -7,8 +7,6 @@ HW10 -- Average
 '''
 
 import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
-
 
 f="info.db"
 
@@ -19,20 +17,29 @@ comm = "SELECT name, peeps.id, mark FROM peeps, courses WHERE peeps.id = courses
 student_list = c.execute(comm)
 #print(student_list)
 
-def info(stuff):
+def info(student):
     nums = 0
     count = 0
-    name = ""
-    idnum = 0
-    for thing in stuff:
-        name = thing[0]
-        idnum = thing[1]
-        nums += thing[2]
+    name = student[0][0]
+    idnum = student[0][1]
+    for grade in student:
+        ''' 
+        debug code================
+        print "name:  "
+        print grade[0]
+        print "ID:    "
+        print grade[1]
+        print "grade: "
+        print grade[2]
+        end of debug code=========
+        '''
+        nums += grade[2]
         count += 1
-    print "Name: " +  name + " ID: " + str(idnum) + " Avg: " + str(nums/count)
+    print "Name: " +  name + "      \tID: " + str(idnum) + "\t\tAvg: " + str(nums/count)
     
 idnum = 1
 entries = []
+#separate student_list into individual students, and process it
 for student in student_list:
     if student[1] == idnum:
         entries.append(student)
@@ -42,11 +49,14 @@ for student in student_list:
         entries = []
         entries.append(entry)
         idnum += 1
+
+#needed for the last student
 info(entries)
 
 
 #==========================================================
-db.commit() #save changes
+#shouldn't be saving any changes, as this is read-only
+#db.commit() #save changes
 db.close()  #close database
 
 
