da854de9887f7cf62c4797f4dfe0a85be7b2bfb4
tmoi29 <tmoi@stuy.edu>
2017-10-17 19:00:54 -0400

    avg working now

--- stu_mean.py
+++ stu_mean.py
@@ -13,9 +13,26 @@ f="info.db"
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
+#print(student_list)
+
 comm = "SELECT name, peeps.id, mark FROM peeps, courses WHERE peeps.id = courses.id;"
 student_list = c.execute(comm)
-#print(student_list)
+
+def avg():
+    comm = "SELECT DISTINCT peeps.id FROM peeps, courses WHERE peeps.id = courses.id"
+    ids = c.execute(comm)
+    for num in ids:
+        comm = "SELECT name, peeps.id, mark FROM peeps, courses WHERE courses.id = " + str(num[0]) + " AND peeps.id = " + str(num[0])
+        student_list = c.execute(comm)
+        total = 0
+        count = 0
+        for student in student_list:
+            total += student[2]
+            count += 1
+        print total/count
+
+avg()
+    
 
 def info(student):
     nums = 0
@@ -37,6 +54,7 @@ def info(student):
         count += 1
     print "Name: " +  name + "      \tID: " + str(idnum) + "\t\tAvg: " + str(nums/count)
     
+'''
 idnum = 1
 entries = []
 #separate student_list into individual students, and process it
@@ -49,9 +67,9 @@ for student in student_list:
         entries = []
         entries.append(entry)
         idnum += 1
-
 #needed for the last student
 info(entries)
+'''
 
 
 #==========================================================
