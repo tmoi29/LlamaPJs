40a1cd7a0ad59602c00b4c9774caccf8d89f8ddd
tmoi29 <tmoi@stuy.edu>
2017-10-16 22:52:34 -0400

    I DID IT

--- stu_mean.py
+++ stu_mean.py
@@ -10,27 +10,32 @@ c = db.cursor()    #facilitate db ops
 comm = "SELECT name, peeps.id, mark FROM peeps, courses WHERE peeps.id = courses.id;"
 student_list = c.execute(comm)
 #print(student_list)
-idnum = 1
-avg = 0
-count = 0
-name = ""
-for student in student_list:
-    print idnum
-    print student[1]
-    if student[1] == idnum:
-        avg += student[2]
+
+def info(stuff):
+    nums = 0
+    count = 0
+    name = ""
+    idnum = 0
+    for thing in stuff:
+        name = thing[0]
+        idnum = thing[1]
+        nums += thing[2]
         count += 1
-        name = student[0]
-    elif count == 0:
-        count = 0
-        avg = 0
-        idnum += 1
+    print "Name: " +  name + " ID: " + str(idnum) + " Avg: " + str(nums/count)
+    
+idnum = 1
+entries = []
+for student in student_list:
+    if student[1] == idnum:
+        entries.append(student)
     else:
-        print "Name: " + name + " ID: " + str(idnum) + " Avg: " + str(avg/count)
-        count = 0
-        avg = 0
+        entry = student
+        info(entries)
+        entries = []
+        entries.append(entry)
         idnum += 1
-        
+info(entries)
+
 
 #==========================================================
 db.commit() #save changes
